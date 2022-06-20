6dd8b2f05a000dc791c468072364066c91769bfb
iwandy <2396691507@qq.com>
2022-06-20 16:50:00 +0800

    测试版本回退

--- src/main/java/com/wang/git/GitTest.java
+++ src/main/java/com/wang/git/GitTest.java
@@ -3,5 +3,6 @@ package com.wang.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("测试git操作");
+        System.out.println("测试git操作  版本回退");
     }
 }
