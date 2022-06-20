41fc93a4991302c7005a77ed4f90eab1b06f6c8f
iwandy <2396691507@qq.com>
2022-06-20 17:07:15 +0800

    测试合并冲突

--- src/main/java/com/wang/git/GitTest.java
+++ src/main/java/com/wang/git/GitTest.java
@@ -3,6 +3,6 @@ package com.wang.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("测试git操作");
-        System.out.println("测试git操作  合并分支");
+        System.out.println("测试git操作  更改master");
     }
 }
